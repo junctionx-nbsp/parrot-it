33c8bc32804988a64e417f639854009abf467ab7
Lukas Tetzlaff <lukastetzlaff@gmail.com>
2018-10-21 02:17:14 +0200

    :hammer: change server address env

--- src/subscribe.ts
+++ src/subscribe.ts
@@ -1,9 +1,9 @@
 import * as WebSocket from "ws"
-import { WS_ADDRESS } from "./utils/envs"
+import { SERVER_ADDRESS } from "./utils/envs"
 
 /**
  * Establish WebSocket Connection with impuAddress in Query
  */
 export default async function subscribe(impuAddress: string) {
-  return new WebSocket(`${WS_ADDRESS}?impuAddress=${encodeURIComponent(impuAddress)}`)
+  return new WebSocket(`ws://${SERVER_ADDRESS}?impuAddress=${encodeURIComponent(impuAddress)}`)
 }
--- src/utils/envs.ts
+++ src/utils/envs.ts
@@ -2,17 +2,17 @@ import { config } from "dotenv"
 config()
 
 const {
-  WS_ADDRESS = ""
+  SERVER_ADDRESS = ""
 } = process.env
 
 if (
   [
-    WS_ADDRESS
+    SERVER_ADDRESS
   ].some(a => a === "")
 ) {
   throw new Error(`Envs not set properly: ${JSON.stringify(process.env)}`)
 }
 
 export {
-  WS_ADDRESS
+  SERVER_ADDRESS
 }
